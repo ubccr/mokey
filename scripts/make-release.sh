c2a20aa1ff280602d2ba239faad091bc5111d799
Andrew E. Bruno <aebruno2@buffalo.edu>
2015-08-28 14:34:01 -0400

    Add script to make releases

---
+++ scripts/make-release.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+MOKEY_DIR='./.mokey-release'
+VERSION=`grep Version main.go | egrep -o '[0-9]\.[0-9]\.[0-9]'`
+REL_DIR=${MOKEY_DIR}/mokey-${VERSION}
+
+rm -Rf ${MOKEY_DIR}
+mkdir -p ${REL_DIR}/{cert,private}
+
+cp ./mokey ${REL_DIR}/ 
+cp ./README.rst ${REL_DIR}/ 
+cp ./AUTHORS.rst ${REL_DIR}/ 
+cp ./LICENSE ${REL_DIR}/ 
+cp -R ./templates ${REL_DIR}/ 
+cp -R ./static ${REL_DIR}/ 
+cp -R ./ddl ${REL_DIR}/ 
+
+tar -C $MOKEY_DIR -cvzf mokey-${VERSION}.tar.gz mokey-${VERSION}
+rm -Rf ${MOKEY_DIR}
