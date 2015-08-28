3a75f05f8b0381b2b49da9a3a6e5a7ac3b270c78
Andrew E. Bruno <aebruno2@buffalo.edu>
2015-08-27 22:50:27 -0400

    Add support for PGP/Mime signed email messages

--- server.go
+++ server.go
@@ -6,9 +6,15 @@ package main
 
 import (
     "fmt"
+    "time"
     "log"
+    "crypto"
     "os"
     "bytes"
+    "net/smtp"
+    "net/textproto"
+    "mime/multipart"
+    "mime/quotedprintable"
     "html/template"
     "encoding/gob"
     "path/filepath"
@@ -16,6 +22,8 @@ import (
     "github.com/jmoiron/sqlx"
     _ "github.com/go-sql-driver/mysql"
 
+    "golang.org/x/crypto/openpgp"
+    "golang.org/x/crypto/openpgp/packet"
     "github.com/Sirupsen/logrus"
     "github.com/carbocation/interpose"
     "github.com/spf13/viper"
@@ -23,7 +31,6 @@ import (
     "github.com/gorilla/schema"
     "github.com/gorilla/sessions"
     "github.com/ubccr/goipa"
-    "gopkg.in/gomail.v2-unstable"
 )
 
 const (
@@ -125,28 +132,164 @@ func NewIpaClient(withKeytab bool) (*ipa.Client) {
     return c
 }
 
+func quotedBody(body []byte) ([]byte, error) {
+    var buf bytes.Buffer
+    w := quotedprintable.NewWriter(&buf)
+    _, err := w.Write(body)
+    if err != nil {
+        return nil, err
+    }
+
+    err = w.Close()
+    if err != nil {
+        return nil, err
+    }
+
+    return buf.Bytes(), nil
+}
+
+func sign(qtext []byte, header textproto.MIMEHeader) ([]byte, error) {
+    var buf bytes.Buffer
+
+    for k, vv := range header {
+        for _, v := range vv {
+            fmt.Fprintf(&buf, "%s: %s\r\n", k, v)
+        }
+    }
+    fmt.Fprintf(&buf, "\r\n")
+    _, err := buf.Write(qtext)
+    if err != nil {
+        return nil, err
+    }
+
+    file, err := os.Open(viper.GetString("pgp_key"))
+    if err != nil {
+        return nil, err
+    }
+
+    keyring, err := openpgp.ReadArmoredKeyRing(file)
+    if err != nil {
+        return nil, err
+    }
+
+    signingKey := keyring[0]
+
+    if signingKey.PrivateKey.Encrypted {
+        err = signingKey.PrivateKey.Decrypt([]byte(viper.GetString("pgp_passphrase")))
+        if err != nil {
+            return nil, err
+        }
+    }
+
+    var sig bytes.Buffer
+    err = openpgp.ArmoredDetachSign(&sig, signingKey, &buf, &packet.Config{DefaultHash: crypto.SHA256})
+    if err != nil {
+        return nil, err
+    }
+
+    return sig.Bytes(), nil
+}
+
 func (a *Application) SendEmail(email, subject, template string, data interface{}) (error) {
     logrus.WithFields(logrus.Fields{
         "email": email,
     }).Info("Sending email to user")
 
     t := a.emails[template]
-    var buf bytes.Buffer
-    err := t.ExecuteTemplate(&buf, template, data)
-
+    var text bytes.Buffer
+    err := t.ExecuteTemplate(&text, template, data)
     if err != nil {
         return err
     }
 
-    m := gomail.NewMessage()
-    m.SetHeader("From", viper.GetString("email_from"))
-    m.SetHeader("To", email)
-    m.SetHeader("Subject", subject)
+    qtext, err := quotedBody(text.Bytes())
+    if err != nil {
+        return err
+    }
 
-    m.SetBody("text/plain", buf.String())
+    header := make(textproto.MIMEHeader)
+    header.Set("Mime-Version", "1.0")
+    header.Set("Date", time.Now().Format(time.RFC1123Z))
+    header.Set("To", email)
+    header.Set("Subject", subject)
+    header.Set("From", viper.GetString("email_from"))
+    header.Set("Content-Type", "text/plain; charset=UTF-8")
+    header.Set("Content-Transfer-Encoding", "quoted-printable")
 
-    d := gomail.Dialer{Host: viper.GetString("smtp_host"), Port: viper.GetInt("smtp_port")}
-    if err := d.DialAndSend(m); err != nil {
+    body := qtext
+
+    if viper.GetBool("pgp_sign") {
+        header.Del("Content-Transfer-Encoding")
+
+        mhead := make(textproto.MIMEHeader)
+        mhead.Add("Content-Type", "text/plain; charset=UTF-8")
+        mhead.Add("Content-Transfer-Encoding", "quoted-printable")
+        sig, err := sign(qtext, mhead)
+        if err != nil {
+            return err
+        }
+
+        var multipartBody bytes.Buffer
+        mp := multipart.NewWriter(&multipartBody)
+        boundary := mp.Boundary()
+        mw, err := mp.CreatePart(mhead)
+        if err != nil {
+            return err
+        }
+        _, err = mw.Write(qtext)
+        if err != nil {
+            return err
+        }
+
+        mw, err = mp.CreatePart(textproto.MIMEHeader(
+            map[string][]string {
+                "Content-Type": []string{"application/pgp-signature; name=signature.asc;"},
+            }))
+        if err != nil {
+            return err
+        }
+
+        _, err = mw.Write(sig)
+        if err != nil {
+            return err
+        }
+
+        err = mp.Close()
+        if err != nil {
+            return err
+        }
+
+        header.Set("Content-Type", fmt.Sprintf(`multipart/signed; boundary="%s"; protocol="application/pgp-signature"; micalg="pgp-sha256"`, boundary))
+        body = multipartBody.Bytes()
+    }
+
+    c, err := smtp.Dial(fmt.Sprintf("%s:%d",viper.GetString("smtp_host"), viper.GetInt("smtp_port")))
+    if err != nil {
+        return err
+    }
+    defer c.Close()
+
+    c.Mail(viper.GetString("email_from"))
+    c.Rcpt(email)
+
+    wc, err := c.Data()
+    if err != nil {
+        return err
+    }
+    defer wc.Close()
+
+    var buf bytes.Buffer
+    for k, vv := range header {
+        for _, v := range vv {
+            fmt.Fprintf(&buf, "%s: %s\r\n", k, v)
+        }
+    }
+    fmt.Fprintf(&buf, "\r\n")
+
+    if _, err = buf.WriteTo(wc); err != nil {
+        return err
+    }
+    if _, err = wc.Write(body); err != nil {
         return err
     }
 
@@ -185,6 +328,7 @@ func (a *Application) router() *mux.Router {
 
 func init() {
     viper.SetDefault("port", 8080)
+    viper.SetDefault("pgp_sign", false)
     viper.SetDefault("smtp_host", "localhost")
     viper.SetDefault("smtp_port", 25)
     viper.SetDefault("email_link_base", "http://localhost")
