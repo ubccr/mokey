7ae57763ba58b10dd9eaa86a358c6556e4f018c7
Aljaž "g5pw" Srebrnič <a2piratesoft@gmail.com>
2018-12-13 00:31:04 +0100

    Implement fully encrypted SMTP connection
    This patch adds the ability to use a fully encrypted connection to the
    SMTP server. Connection type is controlled by the "smtp_tls" config
    value, with three possible options:
    
    - "off": connection is unencrypted (default)
    - "on": connection is fully encrypted
    - "starttls": connection is encrypted on demand via STARTTLS

--- util/pgp.go
+++ util/pgp.go
@@ -11,6 +11,7 @@ import (
 	"fmt"
 	"mime/multipart"
 	"mime/quotedprintable"
+	"net"
 	"net/smtp"
 	"net/textproto"
 	"os"
@@ -39,7 +40,7 @@ func init() {
 	viper.SetDefault("pgp_sign", false)
 	viper.SetDefault("smtp_host", "localhost")
 	viper.SetDefault("smtp_port", 25)
-	viper.SetDefault("smtp_starttls", false)
+	viper.SetDefault("smtp_tls", "off")
 	viper.SetDefault("email_prefix", "mokey")
 	viper.SetDefault("email_link_base", "http://localhost")
 	viper.SetDefault("email_from", "helpdesk@example.com")
@@ -239,13 +240,34 @@ func (e *Emailer) sendEmail(email, subject, tmpl string, data map[string]interfa
 		body = multipartBody.Bytes()
 	}
 
-	c, err := smtp.Dial(fmt.Sprintf("%s:%d", viper.GetString("smtp_host"), viper.GetInt("smtp_port")))
+	smtpHostPort := fmt.Sprintf("%s:%d", viper.GetString("smtp_host"), viper.GetInt("smtp_port"))
+	var conn net.Conn
+	tlsMode := viper.GetString("smtp_tls")
+
+	switch tlsMode {
+	case "on":
+		tlsConfig := &tls.Config{
+			InsecureSkipVerify: false,
+			ServerName:         viper.GetString("smtp_host"),
+		}
+		conn, err = tls.Dial("tcp", smtpHostPort, tlsConfig)
+	case "off", "starttls":
+		conn, err = net.Dial("tcp", smtpHostPort)
+	default:
+		return fmt.Errorf("invalid config value for smtp_tls: %s", tlsMode)
+	}
+
+	if err != nil {
+		return err
+	}
+
+	c, err := smtp.NewClient(conn, viper.GetString("smtp_host"))
 	if err != nil {
 		return err
 	}
 	defer c.Close()
 
-	if viper.GetBool("smtp_starttls") {
+	if tlsMode == "starttls" {
 		err := c.StartTLS(&tls.Config{
 			ServerName: viper.GetString("smtp_host"),
 		})
