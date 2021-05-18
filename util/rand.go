7b39dca5e9de71071c377072e30377ce7c62d903
Andrew E. Bruno <aebruno2@buffalo.edu>
2021-05-18 10:51:03 -0400

    Generate secrets if not set

---
+++ util/rand.go
@@ -0,0 +1,20 @@
+// Copyright 2015 mokey Authors. All rights reserved.
+// Use of this source code is governed by a BSD style
+// license that can be found in the LICENSE file.
+
+package util
+
+import (
+	"crypto/rand"
+	"encoding/hex"
+)
+
+func GenerateSecret(n int) (string, error) {
+	secret := make([]byte, n)
+	_, err := rand.Read(secret)
+	if err != nil {
+		return "", err
+	}
+
+	return hex.EncodeToString(secret), nil
+}
