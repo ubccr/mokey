ac028c9d5b7ea28a2d8a3aa3bca276184ff60ce8
Fabian 'xx4h' Sylvester <xx4h@xx4h.de>
2024-09-22 11:13:44 +0200

    Make user check optional through new config
    option "accounts.check_user"
    
    Default "true", to reflect the current default behaviour of mokey
    
    fix #92

--- server/server.go
+++ server/server.go
@@ -40,6 +40,7 @@ type Server struct {
 func SetDefaults() {
 	viper.SetDefault("site.name", "Acme Widgets")
 	viper.SetDefault("site.ktuser", "mokeyapp")
+	viper.SetDefault("accounts.check_user", true)
 	viper.SetDefault("accounts.default_homedir", "/home")
 	viper.SetDefault("accounts.default_shell", "/bin/bash")
 	viper.SetDefault("accounts.min_passwd_len", 8)
